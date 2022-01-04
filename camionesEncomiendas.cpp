1cdd799875a06992b2f2b841f46e9cb2721c333a
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2022-01-03 21:01:56 -0300

    Update and rename main.cpp to camionesEncomiendas.cpp

--- camionesEncomiendas.cpp
+++ camionesEncomiendas.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+/* CONSIGNA
+Hacer un programa para ingresar los valores de los pesos de distintas
+encomiendas que se deben enviar a distintos clientes y que finaliza cuando se 
+ingresa un peso negativo. Se deben agrupar las encomiendas en camiones que 
+pueden transportar hasta 200 kilos en total. 
+Por ejemplo:
+10, 20, 140, 70, 100, 40, 10, 50, 80, 90, 30, 40, 50, -10.
+Camión 1. Camión 2 Camión 3 Camión 4 Camión 5
+*/
+
 int main()
 {
     //Variables
