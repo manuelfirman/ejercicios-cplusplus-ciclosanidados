a643b4ebd02effc657bf05b0964d6de4458fa05a
Manuel Firman <92244840+manuelfirman@users.noreply.github.com>
2022-01-03 21:05:09 -0300

    Update camionesEncomiendas.cpp

--- camionesEncomiendas.cpp
+++ camionesEncomiendas.cpp
@@ -10,6 +10,13 @@ pueden transportar hasta 200 kilos en total.
 Por ejemplo:
 10, 20, 140, 70, 100, 40, 10, 50, 80, 90, 30, 40, 50, -10.
 Camión 1. Camión 2 Camión 3 Camión 4 Camión 5
+
+Se pide determinar e informar:
+a. El número de camión y peso total de encomiendas (Camión 1: 170kg, 
+Camión 2: 170kg, etc.).
+b. El número de camión que transporta mayor cantidad de encomiendas 
+(en el ejemplo anterior sería el camión 3 con 4 encomiendas).
+c. La cantidad de camiones que se terminaron cargando
 */
 
 int main()
